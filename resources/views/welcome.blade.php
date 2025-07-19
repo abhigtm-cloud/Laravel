c6fdc6d5737010c0d8d5bed34c4891b8c18d1043
abhi <abhigtm3003@gmail.com>
2025-07-19 17:37:04 +0530

    Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abhi</title>
+   @vite(['resources/css/app.css'])
+</head>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <div class="bg-grey-100 p-10 rounded-xl shadow-lg text-center max-w-md w-11/12 border border-gray-200">
+        <h1 class="text-6xl font-extrabold text-gray-600 mb-6 uppercase tracking-wider">
+            Hello
+        </h1>
+    </div>
+</body>
+</html>
